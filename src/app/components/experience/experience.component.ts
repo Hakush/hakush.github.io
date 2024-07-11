24d070402ee7241af3e4b56a1510ca4fcb3d202f
Facundo Sharry <facundo.sharry@mi.unc.edu.ar>
2024-07-11 19:21:17 -0300

    vs

--- src/app/components/experience/experience.component.ts
+++ src/app/components/experience/experience.component.ts
@@ -2,21 +2,21 @@ import { Component, OnInit } from '@angular/core';
 import { PortfolioService } from 'src/app/services/portfolio.service';
 
 @Component({
-  selector: 'app-experience',
-  templateUrl: './experience.component.html',
-  styleUrls: ['./experience.component.css']
+    selector: 'app-experience',
+    templateUrl: './experience.component.html',
+    styleUrls: ['./experience.component.css']
 })
-export class ExperienceComponent implements OnInit{
+export class ExperienceComponent implements OnInit {
+    experiences: any;
 
-  experiences:any;
+    constructor(private datosPortfolio: PortfolioService) {
 
-  constructor(private datosPortfolio:PortfolioService) {
+    }
 
-  }
-
-  ngOnInit():void {
-    this.datosPortfolio.obtenerDatos().subscribe(data => {
-      this.experiences = data.experiencias;
-    });
-  }
+    ngOnInit(): void {
+        // Traer datos de JSON en carpeta assets/
+        this.datosPortfolio.obtenerDatos().subscribe(data => {
+            this.experiences = data.experiencias;
+        });
+    }
 }
