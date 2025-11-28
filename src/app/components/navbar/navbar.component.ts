782c67a1602135bf954d3a39ebaa16ce829e1ed6
Facundo Sharry <facundo.sharry@gmail.com>
2025-11-28 19:08:39 -0300

    dev modified navbar, new animation + new font

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavBarComponent {
+export class NavBarComponent implements OnInit {
+  scrolled = false;
+
+  ngOnInit(): void {
+    this.scrolled = window.pageYOffset > 0;
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll(): void {
+    this.scrolled = window.pageYOffset > 0;
+  }
 }
