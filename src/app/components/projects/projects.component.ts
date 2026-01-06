24ab2b10b1814eadc38f995b63a6e9c6a46a4b86
Facundo Sharry <facundo.sharry@gmail.com>
2026-01-06 11:14:53 -0300

    P0-4 added projects section

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PortfolioService } from 'src/app/services/portfolio.service';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.css']
 })
-export class ProjectsComponent {
+export class ProjectsComponent implements OnInit {
+    protected projects: any;
+
+    constructor(private datosPortfolio: PortfolioService) { }
+
+    ngOnInit(): void {
+        // Traer datos de JSON en carpeta assets/
+        this.datosPortfolio.obtenerDatos().subscribe(data => {
+            this.projects = data.projects;
+            this.projects.reverse();
+        });
+    }
 
 }
